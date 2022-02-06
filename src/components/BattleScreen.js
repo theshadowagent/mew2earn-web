5912faa7dff30c5f694aa87c467eb5bae4d1ada9
theshadowagent <shadowagent1st@gmail.com>
2022-02-06 21:53:52 +0800

    fix: new client

--- src/components/BattleScreen.js
+++ src/components/BattleScreen.js
@@ -5,7 +5,7 @@ export const BattleScreen = () => {
         border: 0,
         width: "44.6vw",
         height: `${44.6 * nintendoSwitchScreenRatio}vw`
-    }} src="https://pokemon-showdown-client-git-feature-metamask-login-buildship.vercel.app/testclient.html" />
+    }} src="https://pokemon-showdown-client-git-develop-buildship.vercel.app/testclient.html" />
 }
 
 export default BattleScreen
